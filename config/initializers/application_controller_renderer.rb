2c9c70ce827dafb1dfa50ad7f0e1fa5119d9bb8c
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-07-20 12:10:52 +0000

    [finish]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '86e98838cb5a675abae9ec845b104b0882ecbbfdcc2a2defe82683337c2e2489d60bd9ce89cd020785232307202ba495a91eac1b0bfbaf97640179242ca3c0c8'
